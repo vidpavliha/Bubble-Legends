c2c9833688ce30e3b64b7ad8b771511a0e89745c
GreenBerry <134782344+vidpavliha@users.noreply.github.com>
2024-07-23 11:03:04 +0200

    i do not know how o fix all of the bugs.

--- main.ts
+++ main.ts
@@ -2,36 +2,32 @@ namespace SpriteKind {
     export const Bubble = SpriteKind.create()
 }
 scene.onOverlapTile(SpriteKind.Bubble, assets.tile`spike up`, function (sprite, location) {
-    tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)), tiles.getTileLocation(1, 6))
+    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).x = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).x
+    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).x = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).y
+    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).x = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).x
+    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).x = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).y
+    pause(200)
+    info.setLife(blockSettings.readNumber("life"))
     info.changeLifeBy(-1)
 })
 scene.onOverlapTile(SpriteKind.Bubble, assets.tile`myTile`, function (sprite, location) {
     tiles.setTileAt(location, assets.tile`transparency16`)
     info.changeScoreBy(1)
 })
-controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
-    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).vy += -100
-    pause(500)
-    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).vy = 30
-})
-function startNextLevel () {
-    if (LvL_state == 1) {
+function level (lvl: number) {
+    if (lvl == 0) {
+        tiles.setCurrentTilemap(tilemap`LvL3`)
+    } else if (lvl == 1) {
         tiles.setCurrentTilemap(tilemap`LvL2`)
-    } else if (LvL_state == 2) {
-    	
-    } else if (LvL_state == 3) {
-    	
-    } else if (LvL_state == 4) {
-    	
-    } else if (LvL_state == 5) {
-    	
-    } else if (LvL_state == 6) {
-    	
+    } else if (lvl == 2) {
+        tiles.setCurrentTilemap(tilemap`LvL4`)
     } else {
-        game.setGameOverEffect(true, effects.starField)
         game.gameOver(true)
     }
 }
+controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).vy += 100
+})
 info.onCountdownEnd(function () {
     game.setGameOverEffect(false, effects.dissolve)
     game.gameOver(false)
@@ -44,10 +40,19 @@ function init () {
     scene.cameraFollowSprite(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)))
 }
 scene.onOverlapTile(SpriteKind.Bubble, assets.tile`finish`, function (sprite, location) {
-    LvL_state = 0
-    LvL_state += 1
-    startNextLevel()
     info.startCountdown(60)
+    animation.runMovementAnimation(
+    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)),
+    animation.animationPresets(animation.flyToCenter),
+    2000,
+    false
+    )
+    animation.runMovementAnimation(
+    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)),
+    animation.animationPresets(animation.flyToCenter),
+    2000,
+    false
+    )
 })
 scene.onOverlapTile(SpriteKind.Bubble, assets.tile`heart`, function (sprite, location) {
     tiles.setTileAt(location, assets.tile`transparency16`)
@@ -58,14 +63,11 @@ info.onLifeZero(function () {
     game.gameOver(false)
 })
 controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
-    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).vy += -100
-    pause(500)
-    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).vy = 30
+    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).vy += 100
 })
-let LvL_state = 0
 init()
 scene.setBackgroundImage(assets.image`background`)
-LvL_state = 0
+let LvL_state = 0
 info.startCountdown(60)
 tiles.setCurrentTilemap(tilemap`LvL1`)
 info.setLife(5)
@@ -76,9 +78,10 @@ if (info.score() == info.highScore()) {
     blockSettings.clear()
 }
 forever(function () {
+    mp.setPlayerIndicatorsVisible(true)
     mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).vy = blockSettings.readNumber("vy")
     mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).vy = blockSettings.readNumber("vy")
-    pause(500)
-    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).vy = blockSettings.readNumber("jumpVy")
-    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).vy = blockSettings.readNumber("jumpVy")
+    blockSettings.writeNumber("life", info.life())
+    blockSettings.writeNumberArray("last pos", [mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).x, mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).y])
+    blockSettings.writeNumberArray("last pos p2", [0, 1])
 })
